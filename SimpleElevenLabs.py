b2656b1fb79889db5db839678b964c016b7e10a0
Vexthecollector <82162107+Vexthecollector@users.noreply.github.com>
2023-05-24 17:45:07 +0200

    Update SimpleElevenLabs.py

--- SimpleElevenLabs.py
+++ SimpleElevenLabs.py
@@ -3,22 +3,56 @@ from elevenlabs.api import History
 import sounddevice as sd
 import soundfile as sf
 import os
+import time
 
-sd.default.device ="CABLE Input (VB-Audio Virtual Cable), Windows Directsound"
-set_api_key("Enter Key Here")
-voices = voices()
-number=0
-for voice in voices:
-    print("[",number,"]",voice.name)
-    number=number+1
-while True:
-    try:   
-        voicenumber=int(input('Select Voice Number:'))
-        break
-    except:
-        print("Please Enter a number")
+voicenumber=0
+
+def Select_Audio_Output():
+    while True:
+        devices=sd.query_devices()
+        number=0
+        for device in devices:
+            if device['max_output_channels'] > 0:
+                print("[",number,"]",device['name'])
+                number=number+1
+        while True:
+            try:   
+                voicenumber=int(input('Select Output Number:'))
+                break
+            except:
+                print("Please Enter a number")
+
+        sd.default.device = voicenumber
+        try:
+            data, fs = sf.read("piano2.wav", dtype="float32")
+            sd.play(data,fs)
+            time.sleep(1)
+            text= input("Could you hear that sound? Type Yes if you did: ") 
+            if text=="Yes" or text=="yes" or text=="Y" or text=="y":
+                break
+        except sd.PortAudioError:
+            print("Looks like that did not work, please select a different output device.")
+
+set_api_key("bf34369e58ba2e9898cae9930dfe11fa")
+def Select_Voice():
+    voicelist = voices()
+    number=0
+    for voice in voicelist:
+        print("[",number,"]",voice.name)
+        number=number+1
+    while True:
+        try:   
+            voicenumber=int(input('Select Voice Number:'))
+            return voicelist[voicenumber]
+            break
+        except:
+            print("Please Enter a number")
+
+Select_Audio_Output()
+selected_Voice=Select_Voice()
+
 def play_Audio():
-    audio = generate(text=newtext, voice=voices[voicenumber])
+    audio = generate(text=newtext, voice=selected_Voice)
     if os.path.exists("currentfile.mp3"):
         os.remove("currentfile.mp3")
     save(audio,"currentfile.mp3")
